cf2c6e9f6d35c7eed6d44a0512461d3392f0f605
joaopajee <162633168+joaopajee@users.noreply.github.com>
2025-06-17 14:27:14 -0300

    17/06

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2025-12-31T00:00:00");
-const tempoObjetivo4 = new Date("2026-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-07-19T00:00:00");
+const tempoObjetivo2 = new Date("2026-07-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-07-31T00:00:00");
+const tempoObjetivo4 = new Date("2026-07-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
